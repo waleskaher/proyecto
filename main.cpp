a7b25bd24a06e45a5b9f89c5c68e7417806cad1e
maryur <maryuri_hernandez94@hotmail.es>
2015-06-10 21:45:33 +0000

    maryury hernandez

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int numero;
+    cout << "ingrese un numero" << endl;
+  cin>>numero;
+
+ if(numero==0)
+cout << "el numero es nulo" << endl;
+ else
+ {
+  if(numero<0)
+      cout << "El numero es NEGATIVO"<< endl;
+  else
+  cout << "El numero es positivo "<< endl;
+ }
+  cout << "creado por MARYURY HERNANDEZ" << endl;
+}
